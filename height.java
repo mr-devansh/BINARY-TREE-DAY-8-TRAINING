ecd50d82714e2fb4368d907a11554a02a48d4a43
devansh singh <66402296+mr-devansh@users.noreply.github.com>
2023-06-23 23:33:31 +0530

    Add files via upload

---
+++ height.java
@@ -0,0 +1,18 @@
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static int height(Node root) {
+        if(root==null)
+            return -1;
+        else{
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+            if(lheight>rheight)
+                return lheight+=1;
+            else
+                return rheight+=1;
+            }
+    }
